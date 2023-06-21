7250a71d612001ec8f4d11011099c776bdb996ad
hulacorn <61078606+uyivzola@users.noreply.github.com>
2023-06-21 23:46:36 +0000

    additing new function traverse_directory

--- file-organizer.py
+++ file-organizer.py
@@ -7,6 +7,28 @@ def get_file_info(file_path):
     """
     file_stats = os.stat(file_path)
     file_size = file_stats.st_size
-    modified_time = datetime.datetime.fromtimestamp(file_stats.st_mtime)
-    created_time = datetime.datetime.fromtimestamp(file_stats.st_ctime)
+    modified_time = dt.datetime.fromtimestamp(file_stats.st_mtime)
+    created_time = dt.datetime.fromtimestamp(file_stats.st_ctime)
     return file_size, modified_time, created_time
+
+def traverse_directory(root_dir):
+    """
+    Traverse a directory tree and perform actions on files.   
+    """
+    for root,directories,files in os.walk(root_dir):
+        print("Current directory: " + root)
+
+        for directory in directories:
+            print('Subdirectory of ' + root + ":" + directory)
+
+            for file in files:
+                file_path=os.path.join(root,file)
+                file_size, modified_time, created_time=get_file_info(file_path)
+
+                print('File in '+root+":" + file)
+                print('File size '+str(file_size)+'bytes')
+                print('Modified time' + str(modified_time))
+                print('Created time '+str(created_time))
+                print('File path ' + os.path.abspath(file_path))
+
+                
